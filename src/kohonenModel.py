ef27239403c0ca4f35b9d3b2e2a781ca8cde8d37
mariano <marperez@itba.edu.ar>
2024-06-02 19:38:12 -0300

    actualizo los vecindarios

--- src/kohonenModel.py
+++ src/kohonenModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import math
 from sklearn.preprocessing import StandardScaler
 
 class Kohonen:
@@ -50,19 +51,71 @@ class Kohonen:
         winner_row = 0
         winner_column = 0
         iterations = 0
+        # desordeno los indices
         shuffled_list = [a for a in range(0, len(self.data))]
         random.shuffle(shuffled_list)
         
         while iterations < self.epochs:
             for i in shuffled_list:
-                winner, winner_row, winner_column = self.get_neuron(i)
+                actualNeuronWinner, winner_row, winner_column = self.get_neuron(i)
                 self.updateNeighbours(winner_row, winner_column, self.data[i], self.learningRate if iterations == 0 else 1/iterations)
-            iterations += 1
-        return winner, winner_row, winner_column
+            iterations +=1
+        return actualNeuronWinner, winner_row, winner_column
+    
+    def update_neuron(self, learningRate, dataRow, neuron):
+        neuron.weights += learningRate * (dataRow-neuron.weights)
         
-    def updateNeighbours(self, i, j, x, learningRate):
-        # busca los vecinos y los actualiza
-        pass
+    def updateNeighbours(self, i, j, dataRow, learningRate):
+        neighbours = self.get_neighbours(i, j)
+        for m in neighbours:
+            self.update_neuron(learningRate, dataRow, m[0])
+            
+    def get_neighbours(self, i, j):
+        neighbours = []
+        #busco los liimites de la red
+        if 0 <= i - self.radius < len(self.neuronsMatrix):
+            bottomLimit = i-self.radius
+        else:
+            bottomLimit = 0
+        if 0 <= i+self.radius < len(self.neuronsMatrix):
+            upperLimit = i + self.radius
+        else:
+            upperLimit = len(self.neuronsMatrix)
+        if 0 <= j + self.radius < len(self.neuronsMatrix[0]):
+            rightLimit = j + self.radius
+        else:
+            rightLimit = len(self.neuronsMatrix[0])
+        if 0 <= j-self.radius < len(self.neuronsMatrix[0]):
+            leftLimit = j - self.radius
+        else:
+            leftLimit = 0
+
+        for p in range(bottomLimit, upperLimit):
+            for n in range(leftLimit, rightLimit):
+                dist = math.sqrt((i-p)**2 + (j-n)**2)
+                if dist <= self.radius:
+                    neighbours.append((self.neuronsMatrix[p][n], p, n))
+
+        return neighbours
+    
+    #busco la neurona para c/fila de dato
+    def get_neuron(self, i):
+        actualNeuronWinner = None
+        neuron = 0
+        row = 0
+        winner_row = row
+        winner_column = neuron
+        minDistance = math.inf
+        for row in range(len(self.neuronsMatrix)):
+            for neuron in range(len(self.neuronsMatrix[row])):
+                # calculo la distancia euclidiana.
+                distance = np.linalg.norm(self.data[i] - self.neuronsMatrix[row][neuron].weights)
+                if distance < minDistance:
+                    minDistance = distance
+                    actualNeuronWinner = self.neuronsMatrix[row][neuron]
+                    winner_row = row
+                    winner_column = neuron
+        return actualNeuronWinner, winner_row, winner_column
     
 class Neuron:
     def __init__(self):
