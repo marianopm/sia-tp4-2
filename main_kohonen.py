547d51bae4f1ca4fd56383060937ecc493f0c75f
mariano <marperez@itba.edu.ar>
2024-06-02 18:35:00 -0300

    cambios kohonen

--- main_kohonen.py
+++ main_kohonen.py
@@ -4,16 +4,17 @@ import pandas as pd
 import numpy as np
 
 def main(): 
-    with open('./config.json', 'r') as f:
+    with open('./config_kohonen.json', 'r') as f:
         data = json.load(f)
 
     data_europe = pd.read_csv('./data/europe.csv', skiprows=1, header=None)
+    data_europe_without_countries = data_europe.iloc[:, 1:]
     data_europe_with_column_names = pd.read_csv('./data/europe.csv')
     
     print(data_europe)
-    
-    network = Kohonen(data_europe, data['radius'], data['k'], data['learningRate'])
-    network.do
 
+    network = Kohonen(data_europe_without_countries, data['radius'], data['k'], data['learning_rate'])
+    network.start()
+    
 if __name__ == "__main__":
     main()
--- src/kohonenModel.py
+++ src/kohonenModel.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 from sklearn.preprocessing import StandardScaler
 
 class Kohonen:
@@ -8,25 +9,71 @@ class Kohonen:
         self.k = k
         self.epochs = k
         self.radius = radius
-        self.standarizedData = self.standarize(self.data)
+        self.learningRate = learningaRate
+        self.function = EuclideanDistance()
+        self.standarized_data = self.standarize(data)
         #Creo las neursonas
-        self.createNeurons()
+        self.neuronsMatrix = self.createNeurons(self.k)
+        self.init_weights(self.neuronsMatrix, data.shape[1])
         
-    def standarize(data):
+        
+    def standarize(self, data):
         scaler = StandardScaler()
-        standarized_data = scaler.fit_transform(data)
-        return standarized_data
+        return scaler.fit_transform(data)
         
-    def createNeurons():
+        
+    def createNeurons(self, k):
         #crea las neuronas
-        #inicio los pesos
+        neurons = []
+        
+        for i in range(k):
+            neuron_row = []
+            for j in range(k):
+                neuron_row.append(Neuron())
+            neuron_row = np.array(neuron_row)
+            neurons.append(neuron_row)
+        neurons = np.array(neurons)
+        
+        return neurons
+    
+    def init_weights(self, neuronsMatrix, dim):
+        for neuron_row in neuronsMatrix:
+            for neuron in neuron_row:
+                neuron.initWeights(dim)
         pass
         
+    def start(self):
+        pass
+    
+    def find_neuron(self):
+        actualNeuronWinner = None
+        winner_row = 0
+        winner_column = 0
+        iterations = 0
+        shuffled_list = [a for a in range(0, len(self.data))]
+        random.shuffle(shuffled_list)
+        
+        while iterations < self.epochs:
+            for i in shuffled_list:
+                winner, winner_row, winner_column = self.get_neuron(i)
+                self.updateNeighbours(winner_row, winner_column, self.data[i], self.learningRate if iterations == 0 else 1/iterations)
+            iterations += 1
+        return winner, winner_row, winner_column
+        
+    def updateNeighbours(self, i, j, x, learningRate):
+        # busca los vecinos y los actualiza
+        pass
+    
 class Neuron:
     def __init__(self):
         self.weights = 0
         self.country = None
         
-    def initWeights(self):
-        self.weights = np.random.uniform(low=-1, high=1, size=(1))
+    def initWeights(self, length):
+        # creo peso random -1,1
+        self.weights = np.random.uniform(low=-1, high=1, size=(length,))
+        
+class EuclideanDistance:
+    def distance(x,y):
+        pass
 
